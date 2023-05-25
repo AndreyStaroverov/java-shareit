b2ce17b23c05631cd4a24d07df4ad80dedbce4aa
Andrey <fanatSai@yandex.ru>
2023-05-25 05:32:00 +0300

    fix

--- src/main/java/ru/practicum/shareit/exceptions/NotOwnerException.java
+++ src/main/java/ru/practicum/shareit/exceptions/NotOwnerException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotOwnerException extends RuntimeException{
+public class NotOwnerException extends RuntimeException {
     public NotOwnerException(String message) {
         super(message);
     }
