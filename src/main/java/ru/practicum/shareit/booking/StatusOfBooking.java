88762d784692d3c879c78093f11388b167700cb5
Andrey <fanatSai@yandex.ru>
2023-05-25 12:44:39 +0300

    fix checkstyle

--- src/main/java/ru/practicum/shareit/booking/StatusOfBooking.java
+++ src/main/java/ru/practicum/shareit/booking/StatusOfBooking.java
@@ -1,11 +1,13 @@
 package ru.practicum.shareit.booking;
+
 /**
- *  Это класс заготовка под следующие спринты,
- *  в данном спринте нам не нужно реализовывать booking & request
+ * Это класс заготовка под следующие спринты,
+ * в данном спринте нам не нужно реализовывать booking & request
  **/
+
 public enum StatusOfBooking {
 
-     WAITING,
+    WAITING,
     APPROVED,
     REJECTED,
     CANCELED
